718277a5792575a99ef90f6412a1efb01ca228fc
mosespace <kisakyemoses50@gmail.com>
2023-06-25 00:08:42 +0300

    feat!imported scss

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
-
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.scss";
+import Navbar from "../components/Navbar";
 
 export const metadata = {
   title: 'Minsk- Webflow HTML Website Clone',
@@ -11,7 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        {/* <Navbar/> */}
+        {children}
+        {/* Footer */}
+      </body>
     </html>
-  )
+  );
 }
