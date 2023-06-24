f8fdef85684ee2ff4daca13d2eebc817aac85f80
mosespace <kisakyemoses50@gmail.com>
2023-06-25 00:09:37 +0300

    feat! added nav-bar

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+"use client";
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <header>
+        <a href="#" className="logo"><img src='https://assets.website-files.com/64183a818a4da828b83b4a85/6418431d8b63796f529bc836_minsk_logo.svg'/></a>
+      <nav>
+        <a href="#">About us</a>
+        <a href="#">Services</a>
+        <a href="#">Blog</a>
+      </nav>
+    </header>
+  );
+}
