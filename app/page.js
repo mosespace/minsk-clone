9de3d3618a642952fda380d7dd8838908e5669c5
mosespace <kisakyemoses50@gmail.com>
2023-06-25 00:09:14 +0300

    feat! initial commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+import Navbar from '@/components/Navbar';
 import React from 'react'
 
 export default function Home() {
   return (
-    <div>page</div>
-  )
+    <main>
+      <Navbar/>
+    </main>
+  );
 }
