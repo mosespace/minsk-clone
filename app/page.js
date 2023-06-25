29016f5d236face8bb9877aa263179f26bdb8ac5
mosespace <kisakyemoses50@gmail.com>
2023-06-25 16:55:46 +0300

    feat! added footer to the page route

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 import React from 'react'
 
 export default function Home() {
   return (
     <main>
       <Navbar/>
+      <Footer/>
     </main>
   );
 }
