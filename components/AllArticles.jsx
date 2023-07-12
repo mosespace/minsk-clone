eb703a1a9091ab4e07671123ce642d8029f03b19
mosespace <kisakyemoses50@gmail.com>
2023-07-12 09:48:45 +0300

    feat: added all articles

--- components/AllArticles.jsx
+++ components/AllArticles.jsx
@@ -6,36 +6,28 @@ export default function AllArticles() {
     {
       id: 1,
       link: "#",
-      image:
-        "https://assets.website-files.com/64183a818a4da873903b4aa2/64186490a9746f7d1387b8cb_pexels-rodnae-productions-7821952-p-500.jpg",
-      w: 331,
-      h: 221,
-      projName: 'Small Bussiness',
-      descrp: '5 Financial Mistakes to Avoid as a Small Business Owner',
-    },
+      projName: 'Financial Statements',
+      descrp: 'Contact us today to learn more about how our accounting services can benefit your business. We look forward to hearing from you and helping you achieve financial success!',
+    }, 
     {
-        id: 2,
-        link: "#",
-        image:
-          "https://assets.website-files.com/64183a818a4da873903b4aa2/6418649c1de30f20cf9c9a21_pexels-rodnae-productions-7821501-p-500.jpg",
-        projName: 'Bookkeeping',
-        descrp: 'The Pros and Cons of Outsourcing Your Payroll',
-      },
-      {
-        id: 3,
-        link: "#",
-        image:
-          "https://assets.website-files.com/64183a818a4da873903b4aa2/641864a796b7dd075b332174_pexels-rodnae-productions-7821675-p-500.jpg",
-        projName: 'Taxation',
-        descrp: 'Understanding Financial Statements: A Beginner"s Guide',
-      },
-  ];
+      id: 2,
+      link: "#",
+      projName: 'Taxes',
+      descrp: 'Contact us today to learn more about how our accounting services can benefit your business. We look forward to hearing from you and helping you achieve financial success!',
+    }, 
+    {
+      id: 3,
+      link: "#",
+      projName: 'Accounting',
+      descrp: 'Contact us today to learn more about how our accounting services can benefit your business. We look forward to hearing from you and helping you achieve financial success!',
+    }, 
+];
 
   return (
     <div className="all-projects">
       {projects.map((projects, i) => {
-        return <ArticlesCard id={i} link={projects.link} width={projects.w} height={projects.h}
-        imgurl={projects.image} name={projects.projName} description={projects.descrp} />;
+        return <ArticlesCard id={i} link={projects.link}
+        title={projects.projName} description={projects.descrp} />;
       })}
     </div>
   );
