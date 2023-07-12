e5acf3159340b16e8ed55128e42712ef406e3af0
mosespace <kisakyemoses50@gmail.com>
2023-07-12 09:48:58 +0300

    feat: added ariticles card

--- components/ArticlesCard.jsx
+++ components/ArticlesCard.jsx
@@ -1,19 +1,37 @@
 'use client'
 import Image from "next/image";
+import { AiOutlineFile } from 'react-icons/ai';
 
-export default function ArticlesCard({ imgurl, name, description, width, height}) {
+export default function ArticlesCard({title, description, link}) {
   return (
-  <div className="all-projects">
-    <div className="card">
-        <Image src={imgurl} alt="Avatar" width={331} height={221} />
-      <div className="infomation">
-        <button>{name}</button>
-        <h1>{description}</h1>
-        <p>Read more</p>
-      </div>
-    </div>
-  </div>
+    <div className="greenCard">
+      <div className="icon">
+        <AiOutlineFile size={30} color="#f6886f" className="icon-size"/>
+      </div>  
+         <h1>{title}</h1>
+         <p className="feedBack">{description}</p>
+             <p className="title-occupation">Read more</p>
+       </div>
   );
 }
+// import React from "react";
+
+// export default function Greencard(props) {
+//   return (
+//     <div className="greenCard">
+//       <h1>“Nonumy eirmod tempor invidunt ut labore et dolore”</h1>
+//       <p className="feedBack">{props.description}</p>
+//       <div className="cardUserDetails">
+//         <div className="user-img">
+//           <Image src={props.Image} alt="" width="50" height="50" />
+//         </div>
+//         <div className="user-details">
+//           <p className="name">{props.name}</p>
+//           <p className="title-occupation">{props.title}</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
 // 331 221
